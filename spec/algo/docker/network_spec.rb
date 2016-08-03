b43ff974974038a037003b2c40dd3e02ee7df7f7
yoshiso <nya060@gmail.com>
2016-08-04 00:52:38 +0900

    Fix enable check duplicate for test

--- spec/algo/docker/network_spec.rb
+++ spec/algo/docker/network_spec.rb
@@ -7,23 +7,24 @@ describe Algo::Docker::Network do
   end
 
   it 'list up all networks' do
-    net = described_class.create({Name: 'mynet1'})
+    net = described_class.create({Name: 'mynet1', CheckDuplicate: true})
     expect(described_class.all.map{ |net| net.name }.to_set)
       .to eq (described_class::DEFAULT_NETWORKS << net.name).to_set
   end
 
   it 'list up networks except default ones' do
-    net = described_class.create({Name: 'mynet2'})
+    net = described_class.create({Name: 'mynet2', CheckDuplicate: true})
     expect(described_class.all(skip_default=true).map{|net| net.name }.to_set)
       .to eq ([net.name]).to_set
   end
 
   it 'create network' do
-    expect { described_class.create({Name: 'mynet3'}) }.not_to raise_error
+    expect { described_class.create({Name: 'mynet3', CheckDuplicate: true}) }
+      .not_to raise_error
   end
 
   it 'remove network' do
-    described_class.create({Name: 'mynet3'})
+    described_class.create({Name: 'mynet3', CheckDuplicate: true})
     expect { described_class.remove('mynet3') }.not_to raise_error
   end
 
